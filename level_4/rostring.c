811342c1b2eb31173f7d265527356ca168bdb0f0
Khomotjo MODIPA <kmodipa@e4r13p2.wtc.co.za>
2017-08-02 16:36:34 +0200

    rostring.c

--- level_4/rostring.c
+++ level_4/rostring.c
@@ -6,76 +6,16 @@
 /*   By: kmodipa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 15:14:41 by kmodipa           #+#    #+#             */
-/*   Updated: 2017/07/24 16:02:17 by kmodipa          ###   ########.fr       */
+/*   Updated: 2017/08/02 16:22:16 by kmodipa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void    ft_putchar(char c)
 {
 	write(1, &c, 1);
-}
-
-int        count_chars(char *str)
-{
-	int    index;
-	int count;
-
-	index = 0;
-	count = 0;
-	while (str[index] != '\0')
-	{
-		if (str[index] != 32 && str[index] != 9)
-		{
-			count++;
-			if (str[index + 1] == 32 || str[index + 1] == 9)
-				break ;
-		}
-		index++;
-	}
-	return (count);
-}
-
-char    *grab_word(char *str)
-{
-	int        len;
-	char    	*word;
-	int        index;
-	int        insert;
-
-	insert = 0;
-	index = 0;
-	len = count_chars(str);
-	word = (char *)malloc(sizeof(word) * (len + 1));
-	while (str[index] != '\0')
-	{
-		if (str[index] != 32 && str[index] != 9)
-		{
-			word[insert] = str[index];
-			insert++;
-			if (str[index + 1] == 32 || str[index + 1] == 9)
-				break ;
-		}
-		index++;
-	}
-	word[insert] = '\0';
-	free(word);
-	return (word);
-}
-
-int        check_index(char *str)
-{
-	int index;
-
-	index = 0;
-	while (str[index] == 32)
-	{
-		index++;
-	}
-	return (index + count_chars(str));
 }
 
 void    ft_putstr(char *str)
@@ -100,40 +40,72 @@ int     ft_strlen(char *str)
 	return (len);
 }
 
-int     end_of_str(char *str)
+char	*ft_strsub(char *str, int start, int end)
 {
-	int        end;
+	char	*sub;
+	int		index;
 
-	end = ft_strlen(str) - 1;
-	while (str[end] == 32)
-		end--;
-	return (end);
+	index = 0;
+	if (!(sub = (char *)malloc(sizeof(sub) * 255)))
+		return (NULL);
+	while (start <= end && str[index])
+		sub[index++] = str[start++];
+	sub[index] = '\0';
+	free(sub);
+	return (sub);
 }
 
-void    rostring(char *str)
+char    *grab_word(char *str)
 {
-	int		index;
 	char	*word;
+	int		start;
+	int		end;
 	int		len;
 
 	len = ft_strlen(str) - 1;
-	word = grab_word(str);
-	index = check_index(str);
+	start = 0;
+	while (str[start] == 32 || str[start] == 9)
+		start++;
+	end = start;
+	while (str[end] != 32 && str[end] != 9 && end <= len)
+		end++;
+	word = ft_strsub(str, start, end - 1);
+	return (word);
+}
+
+int     word_end(char *str)
+{
+	int     start;
+	int     end;
+	int		len;
+
+	len = ft_strlen(str) - 1;
+	start = 0;
+	while (str[start] == 32 || str[start] == 9)
+		start++;
+	end = start;
+	while (str[end] != 32 && str[end] != 9 && end <= len)
+		end++;
+	return (end);
+}
+void    rostring(char *str)
+{
+	int		index;
+	int		end;
+
+	end	= ft_strlen(str) - 1;
+	index = word_end(str);
+	while (str[end] == 32 || str[end] == 9)
+		end--;
+	while (str[index] == 32 || str[index] == 9)
+		index++;
 	while (str[index] != '\0')
 	{
 		if (str[index] != 32 && str[index] != 9)
 		{
 			if (str[index] != 32 && str[index] != 9)
 				ft_putchar(str[index]);
-			if ((str[index + 1] == 32 || str[index + 1] == 9) &&
-					(str[index] != 32 || str[index] != 9))
-			{
-				if (index != end_of_str(str))
-					ft_putstr(" ");
-				else
-					break;
-			}
-			else if (index == end_of_str(str))
+			if (str[index + 1] == 32 || str[index + 1] == 9 || index == end)
 				ft_putchar(32);
 		}
 		index++;
@@ -142,8 +114,6 @@ void    rostring(char *str)
 
 int        main(int c, char **v)
 {
-	char *word;
-
 	if (c < 2)
 	{
 		ft_putchar('\n');
@@ -151,9 +121,8 @@ int        main(int c, char **v)
 	}
 	else
 	{
-		word = grab_word(v[1]);
 		rostring(v[1]);
-		ft_putstr(word);
+		ft_putstr(grab_word(v[1]));
 	}
 	ft_putchar('\n');
 	return (0);
